1d156fa8c5832470b235e9c1caf134548d6402dd
Craig.Turley <craig.j.turley@gmail.com>
2023-11-24 12:13:49 -0800

    Refactors JS file

--- Main/main.js
+++ Main/main.js
@@ -1,176 +1,18 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function(){
 
-    const win_amount = new Map([
+    const win_amount_map = new Map([
         ["Test_Playscreen_sym_01.png", 500],
         ["Test_Playscreen_sym_02.png", 400],
         ["Test_Playscreen_sym_03.png", 300],
         ["Test_Playscreen_sym_04.png", 200],
         ["Test_Playscreen_sym_05.png", 100],
     ]);
-    const reels = document.querySelectorAll(".reel");
-    const btn = document.querySelector('#btn-spin');
-
-    let credits = document.querySelector("#credits");
-    let bet = document.querySelector("#bet");
-    let win = document.querySelector("#win");
-
-    function shuffleArray(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
-        return array;
-    }
-
-    function init() {
-        reels.forEach(reel => {
-            for(let i = 0; i < 4; i++) {
-                reel.innerHTML += `<img src="../Assets/Test_Playscreen_sym_0${Math.floor(Math.random() * 5) + 1}.png" class="img-test">`
-                //reel.innerHTML += `<img src="../Assets/Test_Playscreen_sym_04.png" class="img-test">`
-            }
-            let imgs = reel.querySelectorAll(".img-test");
-            imgs.forEach((img, index) => {
-                img.style.top = `${250 * index}px`;
-                // img.classList.add('transition-effect');
-            });
-        });
-    };
-
-    function checkWin() {
-
-        console.log('Checking win');
-        let middleImages = [];
-        reels.forEach(reel => {
-            let imgs = reel.querySelectorAll(".img-test");
-            imgs.forEach((img) => {
-                if (img.style.top === '500px') {
-                    middleImages.push(img.src);
-                }
-            });
-        });
-
-        let element = middleImages[0];
-        for(let i = 0; i < middleImages.length; i++) {
-            if (element !== middleImages[i]) {
-                win.innerHTML = 0;
-                credits.innerHTML = parseInt(credits.innerHTML) - parseInt(bet.innerHTML);
-                middleElements = [];
-                console.log('lose')
-                return;
-            }
-        }
-
-        console.log('win');
-        let img = element.toString().slice(32, 60);
-        win.innerHTML = (win_amount.get(img)) * parseInt(bet.innerHTML);
-        credits.innerHTML = parseInt(credits.innerHTML) + (win_amount.get(img)) * parseInt(bet.innerHTML);
-        
-        
-        
-        middleImages = [];
-        
-    }
-
-    const preAnimate = (image) => {
-
-        return new Promise((resolve, reject) => {
-
-            image.classList.add('transition-effect');
-            let currentTop = parseInt(image.style.top);
-            image.style.top = `${currentTop + 250}px`;
-
-            setTimeout(resolve, 400);
-
-        });
-
-        
-    };
-
-    async function scrollAll() {
-        btn.style.filter = 'brightness(50%)';
-        await reels.forEach((reel, index) => {
-            let imgs = reel.querySelectorAll(".img-test");
-            imgs.forEach(async (img) => {
-                img.classList.remove('transition-effect'); // Remove the pre-animation class
-                await scroll(img, index);
-                finishScrolling(img, index);
-                btn.style.filter = '';
-            });
-        });
-
-        setTimeout(() => {
-            checkWin();
-            btn.style.filter = '';
-        }, 3500);
-    }
-
-    function scroll(element, offset, random) {
-
-        let duration = 3000;
-        const startTime = performance.now();
-        let delta = (offset + 2) * 7;
-        let currentTop = parseInt(element.style.top);
-        let lastLogTime = 0;
-
-        //element.classList.add('transition-start');
-        //setTimeout(() => {element.classList.remove('transition-effect')}, 1000);
-
-        return new Promise((resolve, reject) => {
-
-        function animate() {
-            const currentTime = performance.now();
-            const elapsedTime = currentTime - startTime;
-
-            if(currentTop >= 1000) {
-                const randomImageNumber = Math.floor(Math.random() * 5) + 1;
-                //const randomImageNumber = 4;
-                element.src = `../Assets/Test_Playscreen_sym_0${randomImageNumber}.png`;
-                currentTop = 0;
-            }
-            else {
-                currentTop += delta;
-            }
-
-            element.style.top = `${currentTop}px`;
-
-            if (elapsedTime < duration) {
-                requestAnimationFrame(animate);
-            }
-            else {
-                resolve();
-            }
-
-        }
-        animate();
     
-        });
-    }
-
-    function finishScrolling(element, index) {
-
-        let currentTop = parseInt(element.style.top);
-
-        if(currentTop % 250 === 0) {
-            return;
-        }
-        // else if (currentTop <= 125) {
-        //     currentTop = 750;
-        // }
-        else {
-            let nearestTop = Math.floor(currentTop / 250) * 250;
-            if (nearestTop === 1000) {
-                nearestTop = 0;
-            }
-            currentTop = nearestTop
-        }
-
-        element.style.top = `${currentTop}px`
-
-    }
-
-    init();
-
-    document.querySelector("#btn-spin").addEventListener("click", scrollAll);
+    const reels = document.querySelectorAll(".reel");
+    const credits = document.querySelector("#credits");
+    const spin_btn = document.querySelector("#btn-spin");
+    const bet = document.querySelector("#bet");
+    const win_amount = document.querySelector("#win"); 
     document.querySelector("#plus").addEventListener("click", () => {
         bet.innerHTML = parseInt(bet.innerHTML) + 1;
     })
@@ -180,4 +22,165 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     })
 
+    const darkenButton = () => {
+        spin_btn.removeEventListener('click', rollAll);
+        spin_btn.style.opacity = '0.5';
+    };
+    
+    
+    const resetButton = () => {
+        spin_btn.addEventListener('click', rollAll);
+        spin_btn.style.opacity = ''; // Reset opacity
+    };
+    
+    const icon_height = 250;
+    const num_icons = 10;
+    const time_per_icon = 100;
+
+    const firstRoll = (reel, delta, offset) => {
+
+        return new Promise((resolve, reject) => {
+            const iconHeight = delta * icon_height
+
+            setTimeout(() => {
+                reel.style.transition = `transform ${4 + delta * time_per_icon}ms ease`;
+                reel.style.transform = `translateY(${iconHeight - 750}px)`;
+            }, offset * 150);
+
+            setTimeout(() => {
+                reel.style.transition = '';
+                first_spin = false;
+                resolve()
+            }, 5500);
+        })
+    }
+
+    const roll = (reel, offset) => {
+
+        // calculate how many icons will spin
+        const delta = (offset + 2) * num_icons + Math.round(Math.random() * num_icons);
+    
+        reel.style.transform = '';
+        reel.style.transition = '';
+    
+        return new Promise((resolve,reject) => {
+            renderIcons(reel, delta)
+            .then(() => {
+                const iconHeight = delta * icon_height;
+            
+                //reel.style.transition = `transform ${4 + delta * time_per_icon}ms ease`;
+                setTimeout(() => {
+                    reel.style.transition = `transform ${4 + delta * time_per_icon}ms ease`;
+                    reel.style.transform = `translateY(${iconHeight - 750}px)`;
+                }, offset * 150)
+            }); 
+    
+            setTimeout(() => {
+                resolve()
+            }, 5500)
+        });
+
+    }
+
+    function renderIcons(reel, delta) {
+        let reelContents = [...reel.querySelectorAll('.img-test')];
+        
+        return new Promise((resolve, reject) => {
+            
+            let currentSymbols = reelContents.slice(0, 3);
+        
+            for (let i = reelContents.length - 3, j = 0; i < reelContents.length; i++, j++) {
+                reelContents[i] = currentSymbols[j].cloneNode(true);
+            }
+        
+            reel.innerHTML = ''; // Clear the current contents
+        
+            for (let j = 0; j < delta - 3; j++){
+                reel.innerHTML += `<img src="../Assets/Test_Playscreen_sym_0${Math.floor(Math.random() * 5) + 1}.png" class="img-test">`
+            }
+            currentSymbols.forEach(img => {
+                reel.appendChild(img);
+            });    
+
+            reel.style.height = `${(delta) * 250}px`;
+            reel.style.top = `-${delta*250 - 750}px`;
+
+            resolve();
+        });
+    }
+    
+    let first_spin = true;
+    let init_delta = [];
+    function rollAll() {
+        darkenButton()
+        if (first_spin === true) {
+            Promise.all([...reels].map((reel, i) => firstRoll(reel, init_delta[i], i)))
+                .then(() => {
+                    console.log('Finished first role');
+                    checkWin(reels);
+                    resetButton();
+                });
+            return;
+        }
+        Promise
+        .all([...reels].map( (reel, i) => roll(reel,i)))
+        .then(() => {
+            console.log("Finished Role");
+            checkWin(reels);
+            resetButton();
+        });
+    }
+
+    const checkWin = (reels) => {
+        let middle = [];
+        reels.forEach((reel) => {
+            middle.push(reel.querySelectorAll(".img-test")[1]);
+        });
+    
+        let win = middle[0].src === middle[1].src && middle[1].src === middle[2].src;
+
+        if (win) {
+            let amountWon = bet.innerHTML * win_amount_map.get(getImageName(middle[0].src)) || 0;
+        
+            credits.innerHTML = parseInt(credits.innerHTML) + amountWon;
+            win_amount.innerHTML = amountWon;
+        
+            
+            let blinkInterval = setInterval(() => {
+                win_amount.style.visibility = win_amount.style.visibility === 'hidden' ? 'visible' : 'hidden';
+            }, 500); 
+        
+            setTimeout(() => {
+                clearInterval(blinkInterval); 
+                win_amount.style.visibility = 'visible'; 
+            }, 3000);
+            return;
+        }
+
+        credits.innerHTML = parseInt(credits.innerHTML) - parseInt(bet.innerHTML);
+    
+    }
+
+    const getImageName = (src) => {
+        return src.substring(src.lastIndexOf('/') + 1);
+    }
+
+    function init() {
+        reels.forEach((reel, i) => {
+            const delta = (i + 2) * num_icons + Math.round(Math.random() * num_icons);
+    
+            for(let j = 0; j < delta; j++) {
+                reel.innerHTML += `<img src="../Assets/Test_Playscreen_sym_0${Math.floor(Math.random() * 5) + 1}.png" class="img-test">`
+            }
+    
+            reel.style.height = `${(delta) * 250}px`;
+            reel.style.top = `${-delta * 250 + 750}px`;
+
+            init_delta.push(delta);
+        });
+    }
+    
+    init()
+    spin_btn.addEventListener('click', rollAll)
+    
 });
